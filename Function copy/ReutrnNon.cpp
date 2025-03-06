c76aaa7be46fb0c7ae9624812134aaeb32c7f09f
setha <sethasith123@gmail.com>
2025-03-06 14:09:55 +0700

    update

---
+++ Function copy/ReutrnNon.cpp
@@ -0,0 +1,80 @@
+// #include<iostream> 
+// using namespace std;
+
+// int ValueX(){
+//     int X;// local variable
+//     X = 10;
+//     return X;
+// }
+// int ValueY(){
+//     int Y;// local variable
+//     Y = 20;
+//     return Y;
+// }
+// float Sum(){
+//     ValueX();
+//     ValueY();
+//     return ValueX() + ValueY();
+// }
+// int main(){
+//     cout<<Sum();
+// }
+
+
+
+#include<iostream>
+using namespace std;
+// create a program for store item
+
+string name;
+int id,qty;
+float price,tax,dis,pay;
+
+
+void InputItem(){
+    cout<<"Enter Item Name: ";
+    cin>>name;
+    cout<<"Enter Item ID: ";
+    cin>>id;
+    cout<<"Enter Item Quantity: ";
+    cin>>qty;
+    cout<<"Enter Item Price: ";
+    cin>>price;
+}
+
+float CalTotal(){
+    return qty * price;
+}
+
+float CalTax(){
+    tax = 0.05;
+    return CalTotal() * tax;
+}
+
+float CalDis(){
+    float total = CalTotal();
+    dis =   (total > 1   && total <=100 ) ? 10 :
+            (total > 100 && total <= 200) ? 20 :
+            (total > 200 && total <= 300) ? 30 : 
+            (total > 300 && total <= 400) ? 40 : 
+            (total > 400) ? 50 :
+            0;
+    return total * dis / 100;
+}
+
+float CalPay(){
+    return CalTotal()-CalDis()+CalTax();
+}
+
+void DisplayItem(){
+      
+}
+
+int main(){
+    system("cls");
+    
+
+
+    return 0;
+
+}
